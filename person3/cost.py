0e66ccbeff5d987849554a24a68ee070d7a358bd
AyuShanaya <101448600+Ayu101-YT@users.noreply.github.com>
2026-02-06 14:26:27 +0530

    fixed error of being able to continue even if flood depth is too much

--- person3/cost.py
+++ person3/cost.py
@@ -1,6 +1,7 @@
 # file to find cost of each route, for a specific car
 # networkx sees which route has lowest cost for a particular car, and then decides which route it will use
 
+from car import Cars 
 
 # finds the speed that the car will travel at in a flood of given depth
 def compute_speed_in_flood(base_speed_kmph, flood_depth_m, loss_rate_per_mm):
@@ -19,8 +20,22 @@ def compute_travel_time(edge_length_m, speed_kmph):
 def compute_flooding_exposure(flood_depth_m, edge_length_m):
     exposure = flood_depth_m * edge_length_m
     return exposure
+# computes the weight(cost) of an edge (road) for the given car, flood depth, road class, and edge length
+def compute_edge_weight(edge_data : dict, vehicle_profile : Cars) -> float: 
+    flood_depth = edge_data.get('flood_depth', 0.0)
+    road_class = edge_data.get('road_class', 'residential')
+    road_multiplier = vehicle_profile.road_multipliers.get(road_class, 1.0)
+    edge_length_m = edge_data.get('length', 100.0)
+    base_speed_kmph = vehicle_profile.base_speed_kmph * road_multiplier
+    flood_speed_kmph = compute_speed_in_flood(base_speed_kmph, flood_depth, vehicle_profile.flood_speed_loss_per_mm)
+    travel_time_s = compute_travel_time(edge_length_m, flood_speed_kmph)
+    exposure = compute_flooding_exposure(flood_depth, edge_length_m)
+    edge_weight = travel_time_s + (exposure * vehicle_profile.flood_penalty_lambda)
+    return edge_weight
 
-def 
+
+
+
     
     
 
